7dfdc5bea27b16c78a3108fd4be10a81a0a85966
caufieldjh <j.harry.caufield@gmail.com>
2023-02-16 14:45:24 -0500

    Incomplete set of valid+invalid examples

--- src/standards_schemas/datamodel/standards_schema_all.py
+++ src/standards_schemas/datamodel/standards_schema_all.py
@@ -1,5 +1,5 @@
 # Auto generated from standards_schema_all.yaml by pythongen.py version: 0.9.0
-# Generation date: 2023-02-15T20:48:36
+# Generation date: 2023-02-16T14:12:42
 # Schema: standards-schema-all
 #
 # id: https://w3id.org/bridge2ai/standards-schema-all
